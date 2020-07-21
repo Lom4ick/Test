0f7e850588afae70802f4d63f0dd185d4510d115
Stanislav Tunda <stanislav.tunda@gmail.com>
2020-07-21 19:53:43 +0300

    add libraries

--- src/main/java/com/company/www/SecondClass.java
+++ src/main/java/com/company/www/SecondClass.java
@@ -1,3 +1,5 @@
+package com.company.www;
+
 public class SecondClass {
     public static void main(String[] args) {
 
--- src/main/java/com/company/www/SomeClass.java
+++ src/main/java/com/company/www/SomeClass.java
@@ -1,3 +1,5 @@
+package com.company.www;
+
 public class SomeClass {
 
     public static void main(String[] args) {
